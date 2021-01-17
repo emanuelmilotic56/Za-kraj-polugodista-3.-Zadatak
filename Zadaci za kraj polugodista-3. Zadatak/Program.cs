567bf621706191fe8481f4a20081e7fee98e99eb
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-01-17 15:22:22 +0100

    Add files via upload

---
+++ Zadaci za kraj polugodista-3. Zadatak/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaci_za_kraj_polugodista_3.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             for (int a = 1; a <= 33; a++)
+             {
+             for (int b = 2; b <= 34; b++)
+             {
+             for (int c = 3; c <= 35; c++)
+             {
+             for (int d = 4; d <= 36; d++)
+             {
+             for (int e = 5; e <= 37; e++)
+             {   
+             for (int f = 6; f <= 38; f++)
+             {
+             for (int g = 7; g <= 39; g++)
+             {
+              Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e + " " + f + " " + g);
+                                    }
+          }
+        } 
+      }
+    }
+  }
+}
+
+
+           Console.ReadKey();
+        }
+    }
+}
+
+
